0c379998693b684dfb27aa22a8ce0676c824eada
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2020-02-06 17:38:03 -0700

    Work in progress

--- src/interfaces/CollectionInterface.php
+++ src/interfaces/CollectionInterface.php
@@ -3,15 +3,19 @@ declare(strict_types=1);
 
 namespace SimpleAcl\Interfaces;
 
-interface CollectionInterface extends \Countable, \IteratorAggregate
-{
+interface CollectionInterface extends \Countable, \IteratorAggregate {
+    
     /**
+     * Remove an item with the specified key from the collection if it exists and return the removed item or null if it doesn't exist.
      * 
      * @param string|int $key
+     * 
+     * @return mixed the removed item
      */
     public function removeByKey($key);
     
     /**
+     * Check if specified key exists in the collection.
      * 
      * @param string|int $key
      * @return bool
