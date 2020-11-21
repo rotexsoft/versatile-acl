6aeda2a360ca8b0e0462bd827e0b316ecb65ab35
rotimi <rotimi@rotimi-VirtualBox>
2020-11-20 20:32:32 -0700

    PHPStorm recommended tweaks

--- src/exceptions/ParentCannotBeChildException.php
+++ src/exceptions/ParentCannotBeChildException.php
@@ -1,9 +1,11 @@
 <?php
 namespace SimpleAcl\Exceptions;
 
+use Exception;
+
 /**
  * Description of ParentCannotBeChildException
  *
  * @author Rotimi
  */
-class ParentCannotBeChildException extends \Exception {}
+class ParentCannotBeChildException extends Exception {}
