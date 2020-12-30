dadd4eefbaf3ef467c3ac3782f6f281b428c6cfc
rotimi <rotimi@rotimi-VirtualBox>
2020-12-30 05:19:14 -0700

    Refactored

--- src/Utils.php
+++ src/Utils.php
@@ -3,11 +3,7 @@ declare(strict_types=1);
 namespace SimpleAcl;
 
 use Closure;
-use Exception;
-use InvalidArgumentException;
-use Throwable;
 use function function_exists;
-use function get_class;
 use function strtolower;
 
 
@@ -28,57 +24,6 @@ class Utils {
 
         return ($callable instanceof Closure)? $callable : Closure::fromCallable($callable);
     }
-
-    /**
-     * 
-     * @param Closure $closure
-     * @param object $newthis
-     * 
-     * @return Closure
-     * @throws InvalidArgumentException
-     */
-    public static function bindObjectAndScopeToClosure(Closure $closure, object $newthis): Closure {
-
-        try {
-            return Closure::bind($closure, $newthis);
-            
-        } catch (Exception $ex) {
-            
-            $function = __FUNCTION__;
-            $class = static::class;
-            $msg = "Error [{$class}::{$function}(...)]: Could not bind \$newthis to the supplied closure"
-                . PHP_EOL . PHP_EOL . static::getThrowableAsStr($ex);
-
-            // The bind failed
-            throw new InvalidArgumentException($msg);
-        }
-    }
-    
-    /**
-     * 
-     * @param Throwable $e
-     * @param string $eol
-     * 
-     * @return string
-     */
-    public static function getThrowableAsStr(Throwable $e, string $eol=PHP_EOL): string {
-
-        $previous_throwable = $e;
-        $message = '';
-
-        do {
-            $message .= "Exception / Error Code: {$previous_throwable->getCode()}"
-                . $eol . "Exception / Error Class: " . get_class($previous_throwable)
-                . $eol . "File: {$previous_throwable->getFile()}"
-                . $eol . "Line: {$previous_throwable->getLine()}"
-                . $eol . "Message: {$previous_throwable->getMessage()}" . $eol
-                . $eol . "Trace: {$eol}{$previous_throwable->getTraceAsString()}{$eol}{$eol}";
-                
-            $previous_throwable = $previous_throwable->getPrevious();
-        } while( $previous_throwable instanceof Throwable );
-        
-        return $message;
-    }
     
     /**
      * 
@@ -87,7 +32,7 @@ class Utils {
      * @return string
      * @noinspection PhpFullyQualifiedNameUsageInspection
      */
-    public static function strtolower(string $str): string {
+    public static function strToLower(string $str): string {
           
         if( function_exists('mb_strtolower') ) {
 
@@ -95,5 +40,17 @@ class Utils {
         }
 
         return strtolower($str);
+    }
+    
+    public static function strSame(string $str1, string  $str2, bool $caseSensitiveComp=true): bool {
+        
+        return $caseSensitiveComp 
+                    ? (\strcmp($str1, $str2) === 0 ) 
+                    : (\strcmp(static::strToLower($str1), static::strToLower($str2)) === 0 );
+    }
+    
+    public static function strSameIgnoreCase(string $str1, string  $str2): bool {
+        
+        return static::strSame($str1, $str2, false);
     }
 }
