969207c2316476837a988131399196892e3ee1d5
rotimi <rotimi@rotimi-VirtualBox>
2020-12-30 05:19:40 -0700

    Refactored

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -48,71 +48,30 @@ class UtilsTest extends \PHPUnit\Framework\TestCase{
             Utils::getClosureFromCallable($anon_func) instanceof Closure
         ); // anonymous function a.k.a Closure
     }
-
-    public function testThatBindObjectAndScopeToClosureWorksAsExpected() {
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable('my_callback_function') instanceof Closure
-        ); // from non-anonymous & non-class function
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable([Ancestor::class, 'who']) instanceof Closure
-        ); // static method call on a class
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable([ (new Descendant() ), 'echoOut']) instanceof Closure
-        ); // instance method call on a class instance
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable(Descendant::class.'::who') instanceof Closure
-        ); // static method call string syntax
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable([Descendant::class, 'parent::who']) instanceof Closure
-        ); // parent class' static method call
-        
-        $this->assertTrue(
-            Utils::getClosureFromCallable( (new Descendant()) ) instanceof Closure
-        ); // instance of class that has __invoke()
-        
-        $anon_func = function($a) {
-            return $a * 2;
-        };
-        $this->assertTrue(
-            Utils::bindObjectAndScopeToClosure($anon_func, $this) instanceof Closure
-        ); // anonymous function a.k.a Closure
-
-        $this->expectException(InvalidArgumentException::class);
-
-        // Code should generate Exception: binding $this to a static Closure
-        Utils::bindObjectAndScopeToClosure(
-            Utils::getClosureFromCallable([Ancestor::class, 'who']),
-            $this
-        );
-    }
-    
-    public function testThatGetThrowableAsStrWorksAsExpected() {
-        
-        $e1 = new DescendantException('Descendant Thrown', 911);
-        $e2 = new AncestorException('Ancestor Thrown', 777, $e1);
-        $e3 = new Exception('Base Thrown', 187, $e2);
-        
-        $ex_as_str = Utils::getThrowableAsStr($e3);
-
-        $this->assertStringContainsString('187', $ex_as_str);
-        $this->assertStringContainsString('Base Thrown', $ex_as_str);
-        $this->assertStringContainsString('777', $ex_as_str);
-        $this->assertStringContainsString('AncestorException', $ex_as_str);
-        $this->assertStringContainsString('Ancestor Thrown', $ex_as_str);
-        $this->assertStringContainsString('911', $ex_as_str);
-        $this->assertStringContainsString('DescendantException', $ex_as_str);
-        $this->assertStringContainsString('Descendant Thrown', $ex_as_str);
-    }
     
     public function testThatStrtolowerWorksAsExpected() {
         
-        $this->assertStringContainsString(Utils::strtolower('Base Thrown'), 'base thrown');
-        $this->assertStringContainsString(Utils::strtolower('777'), '777');
+        $this->assertStringContainsString(Utils::strToLower('Base Thrown'), 'base thrown');
+        $this->assertStringContainsString(Utils::strToLower('777'), '777');
+    }
+    
+    public function testThatStrSameWorksAsExpected() {
+        
+        $this->assertTrue(Utils::strSame('Bob', 'Bob')); // case-sensitive
+        $this->assertTrue(Utils::strSame('777', '777')); // case-sensitive
+        $this->assertTrue(Utils::strSame('Bob', 'bOb', false)); // case-insensitive
+        
+        $this->assertFalse(Utils::strSame('Bob', 'boB')); // case-sensitive
+        $this->assertFalse(Utils::strSame('boB', 'Bob')); // case-sensitive
+        $this->assertFalse(Utils::strSame('boB', 'BoOb', false)); // case-insensitive
+    }
+    
+    public function testThatStrSameIgnoreCaseWorksAsExpected() {
+        
+        $this->assertTrue(Utils::strSameIgnoreCase('Bob', 'bOb'));
+        $this->assertTrue(Utils::strSameIgnoreCase('bOb', 'Bob'));
+        $this->assertFalse(Utils::strSameIgnoreCase('boB', 'B0b'));
+        $this->assertFalse(Utils::strSameIgnoreCase('boB', 'BoOb'));
     }
 }
 
