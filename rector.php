3207dd129d7964d03011cec3a6eaba7760f3d71b
rotimi <rotimi@rotimi-VirtualBox>
2021-05-18 03:42:58 -0600

    Rector recommended enhancements

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -21,9 +20,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     //$parameters = $containerConfigurator->parameters();
 
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
     // register a single rule
-    // $services->set(TypedPropertyRector::class);
+    //$services = $containerConfigurator->services();
+    //$services->set(TypedPropertyRector::class);
 };
